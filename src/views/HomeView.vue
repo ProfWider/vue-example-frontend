4c1ff66f7cdb26b420747913c3cbf2955f2c8392
Prof. Dr. Arif Wider <wider@htw-berlin.de>
2023-06-06 13:08:19 +0200

    adds simple example component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DynamicForm title="Let's talk to the backend!"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import DynamicForm from '@/components/DynamicForm.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    DynamicForm
   }
 }
 </script>
